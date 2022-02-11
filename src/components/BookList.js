00f1a171331191f4520fd35da7998979b9c3d42b
Kirsten S <kirstenjoy@kbsymanzik.org>
2022-02-11 16:34:09 -0500

    set up components for books, list of books, and form to add books, rendered them in app.js

---
+++ src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Book from './Book';
+
+const BookList = () => {
+    return (
+        <>
+            <h1>Books I've Read</h1>
+            <Book />
+        </>
+    )
+}
+
+export default BookList;
