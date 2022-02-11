85ca6a84860be49debb0613a2edf9321b03c2c4f
Kirsten S <kirstenjoy@kbsymanzik.org>
2022-02-11 17:16:22 -0500

    set up form state, set up app.js state, made it so new books could be added to app.js state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import BookList from './components/BookList';
 import BookForm from './components/BookForm';
 
 function App() {
+  const [bookList, setBookList] = useState([]);
+  console.log("bookList", bookList);
+
+  const addBook = book => {
+    const newBook = {
+      title: book.title,
+      author: book.author,
+      pages: book.pages,
+      dateCompleted: book.dateCompleted
+    }
+    setBookList([...bookList, newBook]);
+  }
+
   return (
     <div className="App">
-      <BookForm />
-      <BookList />
+      <BookForm addBook={addBook}/>
+      <BookList bookList={bookList}/>
     </div>
   );
 }
--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,33 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const BookForm = () => {
+const BookForm = (props) => {
+    const [book, setBook] = useState({
+        title: '',
+        author: '',
+        pages: 0,
+        dateCompleted: new Date().toLocaleString()
+    })
+    console.log("book from form", book);
+
+    const handleChanges = e => {
+        setBook({...book, [e.target.name]: e.target.value});
+    };
+
+    const submitForm = e => {
+        e.preventDefault();
+        props.addBook(book);
+        setBook({title: '', author: '', pages: 0, dateCompleted: new Date().toLocaleString()});
+    }
+
     return (
         <>
-            <form>
-                <label htmlFor='title'>Title: </label>
+            <form onSubmit={submitForm}>
+                <label htmlFor='title'><h5>Title: </h5></label>
                 <input 
                     type='text'
                     name='title'
                     id='title'
+                    value={book.title}
+                    onChange={handleChanges}
                 />
-                <label htmlFor='author'>Author: </label>
+                <label htmlFor='author'><h5>Author: </h5></label>
                 <input 
                     type='text'
                     name='author'
                     id='author'
+                    value={book.author}
+                    onChange={handleChanges}
                 />
-                <label htmlFor='pages'>Pages: </label>
+                <label htmlFor='pages'><h5>Pages: </h5></label>
                 <input 
                     type='number'
                     name='pages'
                     id='pages'
+                    value={book.pages}
+                    onChange={handleChanges}
                 />
-                <label htmlFor='dateCompleted'>Date Completed: </label>
+                <label htmlFor='dateCompleted'><h5>Date Completed: </h5></label>
                 <input 
                     type='date'
                     name='dateCompleted'
                     id='dateCompleted'
+                    value={book.dateCompleted}
+                    onChange={handleChanges}
                 />
+                <button type='submit'>Add Book</button>
             </form>
         </>
     )
