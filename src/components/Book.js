330343aafba3728ba994142e480052ea33e3cd8c
Kirsten S <kirstenjoy@kbsymanzik.org>
2022-03-23 16:20:42 -0400

    rendered the book cover to the Book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -10,6 +10,10 @@ const Book = (props) => {
     return (
         <>
             <h3>Title: {props.book.title}</h3>
+            <img 
+                src={props.book.cover_image_url}
+                alt="The book cover"
+            />
             <h4>Author: {props.book.author}</h4>
             <p>Pages: {props.book.page_count}</p>
             <p>Date Completed: {dateCompleted.toLocaleString()}</p>
