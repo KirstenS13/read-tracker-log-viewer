8d1da0f3333c203d0b586c8beac5b5447c151f7a
Kirsten S <kirstenjoy@kbsymanzik.org>
2022-03-23 17:45:55 -0400

    rendered the total time spent reading the book to the Book component in hr min sec format, put all the computations in variables to hopefully make the code more readable

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,10 +3,28 @@ import React from 'react';
 const Book = (props) => {
     const sessions = props.book.sessions
     const timestamp = sessions[sessions.length - 1].timestamp
-    // console.log(timestamp)
     const dateCompleted = new Date(timestamp)
-    // console.log(dateCompleted)
+
+    // go through the sessions array for each book and find the total time spent reading the book
+
+    // props.book.sessions.reduce((previousTotal, object) => previousTotal + object.desired_value, initialValue)
+    const totalReadingTimeInSeconds = props.book.sessions.reduce((total, session) => total + session.duration_seconds,0)
+
+    // wholeHours = the number of complete hours contained in the totalReadingTimeInSeconds
+    const wholeHours = Math.floor(totalReadingTimeInSeconds/3600)
+
+    // wholeHoursInSeconds = wholeHours converted to seconds
+    const wholeHoursInSeconds = wholeHours * 3600
+
+    // wholeMinutes = the number of complete minutes contained in totalReadingTimeInSeconds - wholeHoursInSeconds
+    const wholeMinutes = Math.floor((totalReadingTimeInSeconds - wholeHoursInSeconds) / 60)
+
+    // wholeMinutesInSeconds = wholeMinutes coverted to seconds
+    const wholeMinutesInSeconds = wholeMinutes * 60
     
+    // wholeSeconds = the number of seconds leftover after taking wholeHoursInSeconds and wholeMinutesInSeconds away from totalReadingTimeInSeconds
+    const wholeSeconds = totalReadingTimeInSeconds - wholeHoursInSeconds - wholeMinutesInSeconds
+
     return (
         <>
             <h3>Title: {props.book.title}</h3>
@@ -18,7 +36,8 @@ const Book = (props) => {
             <p>Pages: {props.book.page_count}</p>
             <p>Date Completed: {dateCompleted.toLocaleString()}</p>
             <p>Total Reading Sessions: {props.book.sessions.length}</p>
-            <text>Thoughts: {props.book.closing_remark}</text>
+            <p>Total Time Spent Reading: {wholeHours} hours {wholeMinutes} minutes {wholeSeconds} seconds</p>
+            <p>Thoughts: {props.book.closing_remark}</p>
         </>
     )
 }
