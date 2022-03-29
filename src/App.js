d658d557dbe37fcbccd77e5666af095a4a24e670
Kirsten S <kirstenjoy@kbsymanzik.org>
2022-03-29 15:41:23 -0400

    added useLocalStorage custom hook

--- src/App.js
+++ src/App.js
@@ -6,6 +6,21 @@ import BookForm from './components/BookForm';
 
 import { readingLog } from './data/data.js';
 
+// custom hook to change localStorage
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
+
+  const setValue = value => {
+    setStoredValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+
+  return [storedValue, setValue];
+}
+
 function App() {
   const [readingLogState, setReadingLogState] = useState(readingLog.books);
 
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,7 +3,6 @@ import Book from './Book';
 
 // to use localStorage the readingLogState will need to be saved to localStorage as an array
 // the useEffect hook will need to be used to update the state whenever the localStorage changes
-// the readingLogState should also be moved to App.js since it will need to be accessed by BookForm.js as well
 
 const BookList = props => {
     return (
